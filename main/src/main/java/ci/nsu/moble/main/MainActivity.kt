9c54b71869953943ccc1d5e5b6136d1ddb7b12f6
prokudinaar <prokudinaanna2006.ap@gmail.com>
2026-02-24 10:26:40 +0700

    added compose activity

---
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -0,0 +1,50 @@
+package ci.nsu.moble.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ci.nsu.moble.main.ui.theme.PracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Column(modifier, Arrangement.SpaceBetween, Alignment.CenterHorizontally) {
+        TextField("", onValueChange = {})
+        Text("text")
+    }
+}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    PracticeTheme {
+//        Greeting("Android")
+//    }
+//}
