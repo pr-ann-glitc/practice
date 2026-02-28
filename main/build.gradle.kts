9ff564fc415b5bb4e2f9dfd78961dfab7598050c
Mikhail Sklyar <work@miksklyar.ru>
2026-02-28 22:43:47 +0700

    migrate to version catalog + code cleanup

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -37,8 +37,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    implementation(libs.androidx.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
