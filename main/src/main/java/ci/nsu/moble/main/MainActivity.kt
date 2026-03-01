b493458462a580a6de92f2ef7657b2d6b28c58a5
Anna <prokudinaanna2006.ap@gmail.com>
2026-03-01 18:17:44 +0700

    update libs

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,13 +1,10 @@
 package ci.nsu.moble.main
 
-import android.R
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,11 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,16 +25,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.modifier.modifierLocalProvider
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ci.nsu.moble.main.ui.theme.Blue
 import ci.nsu.moble.main.ui.theme.DarkBlue
 import ci.nsu.moble.main.ui.theme.Green
@@ -106,7 +93,7 @@ fun Main() {
                 buttonColor.value = colorButtonCollection.getOrDefault(userColor.value.lowercase(), Color.Gray)
                 textButtonColor.value = colorTextCollection.getOrDefault(userColor.value.lowercase(), Color.Black)
             if (colorButtonCollection.get(userColor.value.lowercase()) == null)
-                Log.e("My", "Color not found")
+                Log.e("Main", "Color not found")
         },
             Modifier.fillMaxWidth().padding(10.dp),
             colors = ButtonDefaults.buttonColors(
